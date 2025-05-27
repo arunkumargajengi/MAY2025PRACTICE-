6c18a7236edb34f6434c46ed2504a63ccc07f1e7
arunkumargajengi <arunkumar.gajengi@gmail.com>
2025-05-27 13:54:59 +0530

    added one more sop line successfully run

--- HelloDemo/src/HelloDemo1.java
+++ HelloDemo/src/HelloDemo1.java
@@ -3,5 +3,7 @@ public class HelloDemo1 {
 public static void main(String [] args) {
 	System.out.println("hello");
 	System.out.println("welcome to java first code ");
+	
+	System.out.println("success fully run");
 }
 }
